d3b7822c6a9332dd8c8e12658285dd42362f49b3
Sameer Puri <sameer@users.noreply.github.com>
2020-02-02 22:16:31 -0600

    Initial commit

---
+++ top/max10/Manifest.py
@@ -0,0 +1,14 @@
+files = [
+    "max10_top.sv",
+]
+
+modules = {
+    "git": [
+        "git@github.com:hdl-util/hdmi.git::master",
+        "git@github.com:hdl-util/sound.git::master",
+        "git@github.com:hdl-util/vga-text-mode.git::master"
+    ]
+}
+
+fetchto = "../../ip_cores"
+
