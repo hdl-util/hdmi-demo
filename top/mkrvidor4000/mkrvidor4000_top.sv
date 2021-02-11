62ec4f0ae4fae0376095b93240b0df2d3b54e4b7
Sameer Puri <sameer@users.noreply.github.com>
2021-02-11 18:29:10 -0500

    update to align with hdmi master branch

--- top/mkrvidor4000/mkrvidor4000_top.sv
+++ top/mkrvidor4000/mkrvidor4000_top.sv
@@ -4,9 +4,7 @@ module mkrvidor4000_top
 
   // HDMI output
   output [2:0] HDMI_TX,
-  output [2:0] HDMI_TX_N,
   output HDMI_CLK,
-  output HDMI_CLK_N,
   inout HDMI_SDA,
   inout HDMI_SCL,
   input HDMI_HPD
@@ -29,7 +27,7 @@ sawtooth #(.BIT_WIDTH(AUDIO_BIT_WIDTH), .SAMPLE_RATE(AUDIO_RATE), .WAVE_RATE(WAV
 
 logic [23:0] rgb;
 logic [9:0] cx, cy;
-hdmi #(.VIDEO_ID_CODE(4), .DDRIO(1), .AUDIO_RATE(AUDIO_RATE), .AUDIO_BIT_WIDTH(AUDIO_BIT_WIDTH)) hdmi(.clk_pixel_x10(clk_pixel_x5), .clk_pixel(clk_pixel), .clk_audio(clk_audio), .rgb(rgb), .audio_sample_word('{audio_sample_word_dampened, audio_sample_word_dampened}), .tmds_p(HDMI_TX), .tmds_clock_p(HDMI_CLK), .tmds_n(HDMI_TX_N), .tmds_clock_n(HDMI_CLK_N), .cx(cx), .cy(cy));
+hdmi #(.VIDEO_ID_CODE(4), .AUDIO_RATE(AUDIO_RATE), .AUDIO_BIT_WIDTH(AUDIO_BIT_WIDTH)) hdmi(.clk_pixel_x5(clk_pixel_x5), .clk_pixel(clk_pixel), .clk_audio(clk_audio), .rgb(rgb), .audio_sample_word('{audio_sample_word_dampened, audio_sample_word_dampened}), .tmds(HDMI_TX), .tmds_clock(HDMI_CLK), .cx(cx), .cy(cy));
 
 logic [7:0] character = 8'h30;
 logic [5:0] prevcy = 6'd0;
